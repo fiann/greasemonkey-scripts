0719cc09a59c541da2938149c7e20ea0e59b36cd
Fiann O'Hagan <fiann@ohagan.me>
2020-11-24 11:26:56 -0800

    Add custom report formatter

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# greasemonkey-scripts
+# GreaseMonkey Scripts
 GreaseMonkey scripts for Rigor and other tools that make my life a bit easier
+
+
+## To install scripts
+
+Install GreaseMonkey, or a modern clone such as [ViolentMonkey](https://violentmonkey.github.io/).
+
+From the ViolentMonkey dashboard, Add Script and choose Add From URL.
+
+## Script index
+
+**Custom report color formatter.** Rigor displays color thresholds on Executive Dashboards. Custom Reports are much more powerful, and allow charts as well as tables of data. But they are very black and white so let's fix that. As a bonus, the script also sets the window title to match the name of the report, which helps when you have multiple tabs open.
+
+ * [gm-rigor-custom-reports.js](./src/gm-rigor-custom-reports.js)
+ * Raw URL to add to GM: [gm-rigor-custom-reports.js](./raw/main/src/gm-rigor-custom-reports.js)
---
+++ src/gm-rigor-custom-reports.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name        Custom Report tweaks for rigor.com
+// @namespace   Violentmonkey Scripts
+// @match       https://monitoring.rigor.com/reports/*
+// @grant       none
+// @version     1.0
+// @run-at      document-idle
+// @author      Fiann O'Hagan <fiann@thisisoptimal.com>
+// @description 11/23/2020, 2:01:51 PM
+// ==/UserScript==
+
+const budgets = {
+  "Mean First Byte Time" : { green: 0.5, red: 1.0 },
+  "Mean First Contentful Paint Time" : { green: 2.0, red: 4.0 },
+  "Mean Speed Index" : { green: 4300, red: 5800 },
+  "Mean Time to Interactive" : { green: 3.8, red: 7.3 },
+  "Mean Total Image Size" : { green: 800, red: 1000 },
+  "Mean Total Video Size" : { green: 1500, red: 2500 }
+};
+  
+const styles = `
+  <style type="text/css">
+    .budget-button { float: right; background-image: linear-gradient(to right, lawngreen, yellow, red); }
+    .budget-green { background-clip: content-box; background-color: #e3fcef !important; }
+    .budget-yellow { background-clip: content-box; background-color: #fffae5 !important; }
+    .budget-red { background-clip: content-box; background-color: #ffebe5 !important; }
+    .budget-brightred { background-clip: content-box; background-color: crimson !important; color: white; font-weight: bold;  }
+  </style>
+`;
+$("head").append($(styles));
+
+const button = $(`<button class="budget-button">Use budget colors</button>`);
+button.click(setBudgetColors);
+$(".reportHeader .row:first").append(button);
+
+
+// Set window title to match report title
+window.jQuery(() => {
+  let report_title = $("h1").text().trim();
+  console.log("Title", report_title)
+  if (report_title !== "") {
+    document.title = report_title + " - Rigor report";
+  }
+});
+
+
+// Add colors to tables
+function setBudgetColors() {
+  
+  let tables = $(".ReactTable");
+  console.log("Checking " + tables.length + " tables");
+  tables.each((t_idx, t_elem) => {
+    let table = $(t_elem);
+    let header_cells = $(".rt-th", table);
+    let headers = [];
+    header_cells.each((h_idx, h_elem) => {
+      headers[h_idx] = $(h_elem).text().trim();
+    });
+    console.log(headers.join());
+    let rows = $(".rt-tr-group .rt-tr", table);
+    rows.each((r_idx, r_elem) => {
+      let row = $(r_elem);
+      let cells = $(".rt-td", row);
+      cells.each((c_idx, c_elem) => {
+        let metric = headers[c_idx];
+        console.log(`Table ${t_idx}, row ${r_idx}, column ${c_idx}, metric ${metric}`);
+        let budget = budgets[metric];
+        if (budget !== undefined) {
+          let cell = $(c_elem);
+          let text = cell.text().trim();
+          let value = window.parseFloat(text);
+          let color;
+          if (value >= (budget.red * 2)) {
+            color = "budget-brightred";
+          } 
+          else if (value >= budget.red) {
+            color = "budget-red";
+          } 
+          else if (value <= budget.green) {
+            color = "budget-green";
+          } 
+          else {
+            color = "budget-yellow";
+          }
+          console.log(`${value} is ${color}`);
+          cell.html(`<div class="${color}">${text}</div>`);
+        }
+      }); // end for cell
+    }); // end for row
+  });
+}
+
+setTimeout(setBudgetColors, 5000);
